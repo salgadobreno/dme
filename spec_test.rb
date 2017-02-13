5f7dfe17259054c13e57e68f65ed66243712c517
Eduardo Santos Marques <eduardo.marques@avixy.com>
2017-02-13 14:11:46 -0200

    Added the rules concept to the StateMachine class

--- spec_test.rb
+++ spec_test.rb
@@ -3,14 +3,14 @@ require_relative "state_machine.rb"
 
 describe StateMachine, "Maintenance interaction cycle definition" do
   before do
-    @state_machine = StateMachine.new [:inicio, :fim], {:add_index => Proc.new{|x| x.index = x.index + 1}}, {:even_index => Proc.new{|x| x % 2 == 0 ? true : false}}
+    @state_machine = StateMachine.new [:inicio, :fim],
+      {:add_index => Proc.new{|x| x.index = x.index + 1}},
+      {:even_index => Proc.new{|x| x.index % 2 == 0 ? true : false}}
   end
 
   describe "when creating a new state machine configuration" do
       it "should load a machine with the basic states" do
         @state_machine.machine_states.must_equal [:inicio, :fim]
-        # state_machine = StateMachine.new :inicio, :fim
-        # assert_equal state_machine.machine_states, [:inicio]
       end
 
       it "should find the add_index operation" do
@@ -19,6 +19,16 @@ describe StateMachine, "Maintenance interaction cycle definition" do
 
       it "should perform add_index operation" do
         @state_machine.execute(:add_index).must_equal 1
+      end
+
+      it "should find the even_index rule" do
+        @state_machine.rules.must_include :even_index
+      end
+
+      it "should verify even_index rule" do
+        @state_machine.verify(:even_index).must_equal true #analysing 0
+        @state_machine.execute(:add_index)
+        @state_machine.verify(:even_index).must_equal false #analysing 1
       end
   end
 end
--- state_machine.rb
+++ state_machine.rb
@@ -1,14 +1,18 @@
 class StateMachine
-  attr_accessor :machine_states, :operations
+  attr_accessor :machine_states, :operations, :rules, :index
 
   def initialize(*args) #TODO: Separate the arguments to improve readability
     @machine_states = args[0]
     @operations = args[1]
+    @rules = args[2]
     @index = 0
   end
 
   def execute(op)
     @operations[op].call(self)
   end
+
+  def verify(rule)
+    @rules[rule].call(self)
   end
 end
