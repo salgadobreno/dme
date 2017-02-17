693140dbe96bbb3dacb61dfa858b76e8963fe5da
eru_dev <gitlab@avixy.com>
2017-02-17 18:52:11 -0200

    completando os testes p/ cobrir todo o cenario proposto, mas não está implementado ainda

--- Gemfile
+++ Gemfile
@@ -2,4 +2,7 @@ source 'https://rubygems.org'
 
 gem 'bundler', '~> 1.14', '>= 1.14.4'
 gem 'minitest', '~> 5.10', '>= 5.10.1'
+gem "minitest-autotest"
+gem "autotest-suffix"
+gem "autotest-spec"
 gem "mocha"
---
+++ state.rb
@@ -0,0 +1,33 @@
+class State
+  attr_reader :name
+
+  def initialize(name, state_options)
+    @name = name
+
+    # register callbacks
+    @validation_callbacks = state_options[:validation]
+    @execution_callbacks = state_options[:execution]
+  end
+
+  def execute(payload)
+    unless @execution_callbacks.nil? || @validation_callbacks.empty?
+      @execution_callbacks.each {|eb| eb.call(payload)}
+    end
+  end
+
+  def validate(payload)
+    r = nil
+
+    if @validation_callbacks.nil? || @validation_callbacks.empty?
+      r = true
+    else
+      # chama all? em array de Boolean, se algum falhar, retorna falso
+      r = @validation_callbacks.map { |validation|
+        validation.call(payload)
+      }.all?
+    end
+
+    return r
+  end
+
+end
