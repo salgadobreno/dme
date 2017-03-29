b10d66443a8dd1329b2349625b24a7315f66887e
Eduardo Santos Marques <eduardo.marques@avixy.com>
2017-03-29 14:54:52 -0300

    Added the mongoID implementation to the project and created a simple test to verify the database connection

--- test/basic_database_test.rb
+++ test/basic_database_test.rb
@@ -1,7 +1,42 @@
 require "irb"
 require "minitest/autorun"
 require "mocha/mini_test"
+require "mongoid"
+require "database_cleaner"
+
+class TestModel
+  include Mongoid::Document
+
+   field :name
+end
 
 describe "Database configuration" do
+  $db_conn = nil
 
+  before do
+    if $db_conn.nil?
+      #cfg_file = "#{$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))}/mongoid.yml"
+      cfg_file = "#{File.expand_path(File.dirname(__FILE__))}/mongoid.yml"
+      $db_conn = Mongoid.load! cfg_file, :development
+    end
+
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.start
+  end
+
+  after do
+    DatabaseCleaner.clean
+  end
+
+  describe "testing a basic connection" do
+    it "should have something in the conn object" do
+      refute_nil $db_conn, "The database server is running?"
+    end
+
+    it "shoudl create a new model object and save it to the database" do
+      model = TestModel.new
+      model.name = "Fluflu"
+      assert model.save!, true
+    end
+  end
 end
