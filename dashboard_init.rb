56ea53ec8cb24f4f6d03b0dc58280810cdde1b00
Eduardo Santos Marques <eduardo.marques@avixy.com>
2017-04-05 12:46:07 -0300

    Added test configuration for the database into the dashboard init.

--- dashboard_init.rb
+++ dashboard_init.rb
@@ -1,5 +1,19 @@
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)) + "/app")
+ENV["MONGODB_CFG_PATH"] ||= File.expand_path(File.dirname(__FILE__)) + "/config/mongoid.yml"
+
+@db_environment = :development
+if ENV["APP_ENV"].nil?
+  ENV["APP_ENV"] = "test"
+end
+
+if ENV["APP_ENV"] == "test"
+  @db_environment = :development
+elsif ENV["APP_ENV"] == "development"
+  @db_environment = :development
+else
+  @db_environment = :production
+end
 
 require 'irb'
 require 'awesome_print'
@@ -9,3 +23,8 @@ require 'app/buffer'
 require 'app/device'
 require 'app/mock_db'
 require 'app/app_log'
+
+
+# configure the database
+Mongoid.load! ENV['MONGODB_CFG_PATH'], @db_environment
+
