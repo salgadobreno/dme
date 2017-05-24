f045dbf663ce4881e4995c327871c4a3a61e084a
Breno <breno.salgado@avixy.com>
2017-05-24 17:47:44 +0000

    validations in the defined states

--- app/states.rb
+++ app/states.rb
@@ -2,13 +2,13 @@ require 'dashboard_init'
 
 class StateAceptance < State
   def initialize
-    super :aceptance, {}
+    super :aceptance, {validations: [BlacklistValidation.new]}
   end
 end
 
 class StateTriage < State
   def initialize
-    super :triage, {}
+    super :triage, {validations: [WarrantyCheckValidation.new]}
   end
 end
 
@@ -20,6 +20,6 @@ end
 
 class StateSegregated < State
   def initialize
-    super :segregated, {:validations => [BetterCallAlineValidation.new]}
+    super :segregated, {validations: [BetterCallAlineValidation.new]}
   end
 end
