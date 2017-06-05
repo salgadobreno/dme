905ab63280e51230781ab7bb50c5fdca256b1345
Breno Salgado <breno.salgado@avixy.com>
2017-06-05 15:24:44 -0300

    fix app runner load path

--- app/app_runner.rb
+++ app/app_runner.rb
@@ -1,3 +1,5 @@
+#$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__), '../'))
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__)) + '/../'
 require 'dashboard_init'
 require 'app/app_service'
 
