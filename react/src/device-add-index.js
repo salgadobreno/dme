ac384c8106c2a6fbf1698926658aba0277338737
Breno Salgado <breno.salgado@avixy.com>
2017-06-19 10:52:20 -0300

    control <PayloadInput> from <AddDevice>, add submit functionality

--- react/src/device-add-index.js
+++ react/src/device-add-index.js
@@ -3,42 +3,26 @@ import { render } from 'react-dom';
 
 //payload input component
 class PayloadInput extends Component {
-  constructor(){
-    super();
-    //this.addInput = this.addInput.bind(this);
-    //this.handleChange = this.handleChange.bind(this);
+  constructor(props){
+    super(props);
   }
-
-  addInput(){
-    this.setState({
-      payload: this.state.payload.concat([["",""]])
-    });
-  }
-
-  //handleChange(event){
-    //const target = event.target;
-    //const value = target.value;
-    //const name = target.name;
-
-    //this.setState({
-      //[name]: value
-    //})
-  //}
 
   render() {
+    const inputs = this.props.payload.map((arr, i)=> {
+      return (
+        <div key={i}>
+          <input type="text" value={arr[0]} onChange={this.props.payloadOnChange.bind(this, i, 0)} />
+          :
+          <input type="text" value={arr[1]} onChange={this.props.payloadOnChange.bind(this, i, 1)} />
+          <input type="button" value="-" onClick={this.props.removePayload.bind(this, i)}/>
+        </div>
+    )
+    });
     return (
       <div>
-        {
-          Object.keys(this.props.value).map((key, i)=> {
-            return <div key={'div-'+i}>
-              <input  name="payload[][key]" type="text" value={key} />
-              :
-              <input  name="payload[][value]" type="text" value={this.props.value[key]} />
-            </div>
-          })
-        }
+        { inputs }
         <div>
-          <input type="button" value="+" onClick={this.addInput}/>
+          <input type="button" value="+" onClick={this.props.addPayloadItem}/>
         </div>
       </div>
     )
@@ -52,40 +36,72 @@ class AddDevice extends Component {
     this.state = {
       id: "",
       serial_number: "",
-      payload: {
-        key1:"value1",
-        key2:"value2"
-      }
+      payload: [
+        ["",""],
+        ["",""]
+      ]
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handlePayloadChange = this.handlePayloadChange.bind(this);
+    this.removePayloadItem = this.removePayloadItem.bind(this);
+    this.addPayloadItem = this.addPayloadItem.bind(this);
+  }
+
+  addPayloadItem(){
+    this.setState({
+      payload: this.state.payload.concat([["",""]])
+    });
+  }
+
+  removePayloadItem(index){
+    const newPayload = this.state.payload.slice();
+    newPayload.splice(index, 1);
+    this.setState({
+      payload: newPayload
+    })
   }
 
   handleSubmit(event){
-    console.log(this.state);
     event.preventDefault();
+
+    const jsonParams = JSON.stringify(this.state);
+
+    fetch('http://localhost:8080/devices', {method: 'post', body:jsonParams, headers: {'Content-Type':'application/json'},}).then(r=> {
+      console.log("response: ");
+      console.log(r);
+      //TODO: verify response code, exception case, etc
+      window.location = "/device-list-index.html";
+    })
   }
 
   handleChange(event){
     const target = event.target;
     const value = target.value;
     const name = target.name;
-
     this.setState({
       [name]: value
+    })
+  }
+
+  handlePayloadChange(index, kv, event){
+    const newPayload = this.state.payload.slice();
+    const modifiedItem = newPayload[index];
+    modifiedItem[kv] = event.target.value;
+    this.setState({
+      payload: newPayload
     })
   }
 
   render(){
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="number" name="id" onChange={this.handleChange} value={this.state.id}/>
         <label>
           Serial Number:
           <input type="text" name="serial_number" onChange={this.handleChange} value={this.state.serial_number}/>
         </label>
-        <PayloadInput value={this.state.payload}/>
+        <PayloadInput payload={this.state.payload} addPayloadItem={this.addPayloadItem} payloadOnChange={this.handlePayloadChange} removePayload={this.removePayloadItem} />
         <input type="submit" value="Submit"/>
       </form>
     )
