d2d1d99429115b5b9695c4a96557a3f943142aab
Breno Salgado <breno.salgado@avixy.com>
2017-06-19 11:21:11 -0300

    explain proc_serializer

--- app/lib/proc_serializer.rb
+++ app/lib/proc_serializer.rb
@@ -1,6 +1,10 @@
 require 'sourcify'
 require 'app/core/models/state'
 
+# Allows for storing Procs/Lambdas in database so Operations/Validations can be defined
+# and stored more easily. This method *WON't be the preffered way* since the backing library
+# has bugs and is unmaintained, but can be used to speed up development/prototyping. If an
+# Operation/Validation is production-ready, it should be made into a class instead.
 class ProcSerializer
 
   def initialize(a_block)
