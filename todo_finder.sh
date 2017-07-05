a4f0a33a560481338a3c583d8db9e8674c3660bb
Edson Ma <edson@avixy.com>
2017-07-05 12:06:57 -0300

    fix for linux grep

--- todo_finder.sh
+++ todo_finder.sh
@@ -2,4 +2,4 @@
 # Created July 4th. - Edson Lek Hong Ma
 #
 
-grep -rni --exclude-dir={*node_modules*,*app/web/public*,*.git*}  '[//|#]*todo' .
+grep -rni --exclude-dir={node_modules,public,.git}  '[//|#]*todo' .
