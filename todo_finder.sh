e86ef9cde9838f9510236853e6817bdc081b6b74
Edson Ma <edson@avixy.com>
2017-07-06 17:26:48 -0300

    result is now aligned in columns

--- todo_finder.sh
+++ todo_finder.sh
@@ -2,4 +2,7 @@
 # Created July 4th. - Edson Lek Hong Ma
 #
 
-grep -rni --exclude-dir={node_modules,public,.git}  '[//|#]*todo' .
+grep --colour=always -rni --exclude-dir={node_modules,public,.git}  '[//|#]*todo' .
+| sed -re  's/^([^:]+):(\x1b\[m\x1b\[K)[[:space:]]*(.*)/\1\x01\2\3/' |
+| sed -re  's/^([^#]+):(\x1b\[m\x1b\[K)[[:space:]]*(.*)/\1\x01\2\3/' |
+| column -s $'\x01' -t
