6623860b9eb05e0fb4cba76662ceb0065a93c23d
Edson Ma <edson@avixy.com>
2017-07-14 16:49:39 -0300

    cleanup DeviceListIndex

--- react/src/device-list-index.js
+++ react/src/device-list-index.js
@@ -5,28 +5,14 @@ import AmDeviceList from './components/AmDeviceList';
 import DeviceList from './components/DeviceList';
 
 class DeviceListIndex extends Component {
-  constructor(){
-    super();
-    this.state = {
-      error: undefined
-    };
-  }
-
   render() {
     return (
       <div className={"row"}>
-      {
-        this.state.error && <Message message={this.state.error}/>
-      }
-
-      <div className={"col-6"}> <AmDeviceList/> </div>
-      <div className={"col-6"}> <DeviceList/> </div>
+        <div className={"col-6"}> <AmDeviceList/> </div>
+        <div className={"col-6"}> <DeviceList/> </div>
       </div>
     );
   }
 }
-
-export default DeviceListIndex
-
 
 render(<DeviceListIndex />, document.getElementById('root'));
